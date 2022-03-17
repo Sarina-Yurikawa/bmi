cadedc32461ccf613c525e6c7511aa7e4a4846c8
Sarina Yurikawa <s.yurikawa.sys21@morijyobi.ac.jp>
2022-03-17 15:21:19 +0900

    BIMを二回表示

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+        public static void main(String[] args){
+                Scanner s = new Scanner(System.in);
+                double sintyo, taiju, bmi;
+                int a;
+                for(a = 0;a <= 1;a++){
+                System.out.println("cm");
+                sintyo = s.nextDouble();
+                System.out.println("kg");
+                taiju = s.nextDouble();
+                sintyo = sintyo/100;
+                bmi = taiju/(sintyo*sintyo);
+                System.out.println("BMI="+(String.format("%.1f", bmi))+"");
+                }
+       }
+}
+
